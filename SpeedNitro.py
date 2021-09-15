17fa979d22c4253d32ad56e47ea8d78dedc2dbd1
igor parak <32526701+PackedUP32@users.noreply.github.com>
2021-09-15 11:37:03 -0700

    Version 2.0 Release

--- SpeedNitro.py
+++ SpeedNitro.py
@@ -1,10 +1,34 @@
+##############################################
+#               |SPEEDNITRO™️|                #
+#     /LICENSED UNDER " GITLICENSE 2.0©️ "\   #
+##############################################
+
+# Load First #
+import sys
+from sys import platform
+
+# Load Library #
+def clear():
+    if platform == "linux" or platform == "linux2":
+        os.system('clear')
+    elif platform == "darwin" or platform == "darwin2":
+        os.system('clear')
+    elif platform == "win32" or platform == "win64":
+        os.system('cls')
+
+
 # Module Library #
-print("\033[1;35;40m Loading modules  \n")
+import os
+import colorama
+from colorama import Fore, Back, Style
+clear()
+print("\033[1;35;40m RESET  \n")
+clear()
+print("\033[1;36;40m Loading modules  \n")
+print("\033[1;36;40m Importing OS  \n")
 print("\033[1;36;40m Importing time  \n")
 import time
 time.sleep(0.0)
-print("\033[1;36;40m Importing OS  \n")
-import os
 import ctypes
 time.sleep(0.4)
 print("\033[1;36;40m Importing Requests  \n")
@@ -12,6 +36,7 @@ import requests
 time.sleep(0.4)
 print("\033[1;36;40m Importing sys  \n")
 import sys
+from sys import platform
 time.sleep(0.4)
 print("\033[1;36;40m Importing Discord  \n")
 time.sleep(0.4)
@@ -27,62 +52,52 @@ import discord
 import urllib3
 import threading
 from threading import Thread
-import colorama
-from colorama import Fore, Back, Style
 import subprocess
 from subprocess import call
 from subprocess import *
 time.sleep(0.4)
 print("\033[1;36;40m Modules Loaded...  \n")
 time.sleep(1)
-os.system("cls")
+clear()
 print("\033[1;36;40m Loading Definers...  \n")
+
 
 # Definer #
 def gen(): # Gen
     # Stage 1
-    os.system("cls")
-    print("generating 16Byte nitro codes")
+    clear()
     codelen = 16
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    print("How many 16byte codes to generate (To not max out CPU/RAM Usage")
-    print("This program will need to generate 16 byte codes, 4 byte codes , 20 byte codes")
-    print("This is to make sure you are getting a higher ammount of getting a code then chancing it on 16 letters")
-    lp = int(input(""))
+    lp = int(input("How many 16 Character codes do you want to generate >> "))
     print("\033[1;32;40m Generating Codes (Press CTRL+C to end)  \n")
     k = open('16bytecodes.txt', 'w')
     for i in range(lp):
         k.write("discord.gift/" + ''.join(random.choice(letters) for i in range(codelen)) + '\n')
     k.close()
     # Stage 2
-    os.system("cls")
+    clear()
     codelen = 4
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    print("generating 4Byte nitro codes")
-    print("How many 4byte codes to generate (To not max out CPU/RAM Usage")
-    lp = int(input(""))
+    lp = int(input("How many 4 Character codes do you want to generate >> "))
     print("\033[1;32;40m Generating Codes (Press CTRL+C to end)  \n")
     k = open('4bytecodes.txt', 'w')
     for i in range(lp):
         k.write("discord.gift/" + ''.join(random.choice(letters) for i in range(codelen)) + '\n')
     k.close()
-    print("generating 20Byte nitro codes")
     # Stage 3
-    os.system("cls")
+    clear()
     codelen = 20
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    print("How many 20byte codes to generate (To not max out CPU/RAM Usage")
-    lp = int(input(""))
+    lp = int(input("How many 20 Character codes do you want to generate >> "))
     print("\033[1;32;40m Generating Codes (Press CTRL+C to end)  \n")
     k = open('20bytecodes.txt', 'w')
     for i in range(lp):
         k.write("discord.gift/" + ''.join(random.choice(letters) for i in range(codelen)) + '\n')
     k.close()
     boot()
-
 def clearfile(): # Clear file
-    os.system("cls")
-    print("clearing file")
+    clear()
+    print("Deleting Files:")
     if os.path.exists("16bytecodes.txt"):
         os.remove("16bytecodes.txt")
         os.remove("4bytecodes.txt")
@@ -102,45 +117,52 @@ def chk(): # Check Codes #
     threading.Thread(target=stage1).start()
     threading.Thread(target=stage2).start()
     threading.Thread(target=stage3).start()
-    # Run check 1 #
+
+#.old (  url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"  )
+
+# Stage 1 #
 def stage1():
     with open("16bytecodes.txt") as f:
         for line in f:
             nitro = line.strip("\n")
-            url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+            url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
             r = requests.get(url)
             if r.status_code == 200:
                 print("\033[1;31;40m Valid | {} \n".format(line.strip("\n")))
                 break
             else:
                 print("\033[1;31;40m Invalid | {} \n".format(line.strip("\n")))
-    # Stage 2 #
+
+# Stage 2 #
 def stage2():
     with open("4bytecodes.txt") as f:
         for line in f:
             nitro = line.strip("\n")
-            url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+            url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
             r = requests.get(url)
             if r.status_code == 200:
                 print("\033[1;31;40m Valid | {} \n".format(line.strip("\n")))
                 break
             else:
                 print("\033[1;31;40m Invalid | {} \n".format(line.strip("\n")))
+
 # Stage 3 #
 def stage3():
     with open("20bytecodes.txt") as f:
         for line in f:
             nitro = line.strip("\n")
-            url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+            url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
             r = requests.get(url)
             if r.status_code == 200:
                 print("\033[1;31;40m Valid | {} \n".format(line.strip("\n")))
                 break
             else:
-                print("\033[1;31;40m Invalid | {} \n".format(line.strip("\n")))            
+                print("\033[1;31;40m Invalid | {} \n".format(line.strip("\n")))
+                
 def menu(): # Logo
-    os.system("cls")
-    ctypes.windll.user32.MessageBoxW(0, "Thank you for using NitroBoost!", "Enjoy!", 0)
+    clear()
+    if platform == "win32" or platform == "win64":
+        ctypes.windll.user32.MessageBoxW(0, "Thank you for using NitroBoost!", "Enjoy!", 0)
     print("\033[1;36;40m  \n")
     print("""
    _______  ___________  _  _______________  ____ 
@@ -149,37 +171,39 @@ def menu(): # Logo
 /___/_/  /___/___/____/_/|_/___/ /_/ /_/|_|\____/ 
 Premium NitroGenerator
 By : PackedUP32
-Version 1.5
+Version 2.0
 Mode - RELEASE (SOURCE)
     """)
 
 def don():
-    os.system("cls")
-    print("Buy me a coffee?")
-    print("Bitcoin = bc1qj4q6352ypmusyghyyp68q02l8hxvhvnyjnj39j")
-    print("Bitcoin Cash = bitcoincash:qq0344acxvjl9xy0vlftl38awjmg3mhz5qcwsgfsd5")
-    print("Xrdp = rBSPTqYrsSkjCwk8AUfpa3eVYczX4GDGmN")
-    print("Dodge Coin = DDZBYK8JdTA2umQnmpSH7n89pfqujdfXg1")
-    print("Ethereum = 0xE343279109A1Bf25f1A888E5B45c55EeD7245174")
-    print("Thank you")
+    clear()
+    print("Donation links & addresses are on main github")
     input("Press enter to continue")
     boot()
+
+def system_check():
+    if platform == "linux" or platform == "linux2":
+        print("\033[1;33;40mOS=> Linux: Testing")
+    elif platform == "darwin" or platform == "darwin2":
+        print("\033[1;33;40mOS=> MacOS: Testing")
+    elif platform == "win32" or platform == "win64":
+        print("\033[1;32;40mOS=> Windows: Supported")
 
 def options(): # Options
     print("\033[1;35;40m [1] Nitro Generator \n")
     print("\033[1;35;40m [2] Nitro Checker \n")
     print("\033[1;35;40m [3] Donate (Please donate so i can do more work like this) \n")
-    print("\033[1;35;40m [4] Clear codes.txt file \n")
-    print("\033[1;35;40m [5] Help Document \n")
-    time.sleep(1)
+    print("\033[1;35;40m [4] Delete Codes \n")
+    print("\033[1;35;40m [5] Updates & Changes \n")
+    time.sleep(0.1)
     print("===================================")
     time.sleep(0.1)
+    print("\033[1;36;40m System Information: ")
+    system_check()
     print("Status > [ WORKING ]")
-    time.sleep(0.1)
-    print("\033[1;35;40m Searching for proxies > [ FOUND ] \n")
-    time.sleep(0.2)
+    print("")
     print("\033[1;35;40m Select your options \n")
-    user_name = input()
+    user_name = input("\033[1;33;40m >> \033[1;32;40m")
     if user_name == "1":
         gen()
 
@@ -193,12 +217,14 @@ def options(): # Options
         clearfile()
 
     elif user_name == "5":
-        os.system("cls")
-        with open("20bytecodes.txt") as f:
-            lines = f.readlines()
+        clear()
+        f = open("update_log.txt")
+        print(f.read())
+        print("")
+        print("")
+        print("Press ENTER to go back.")
         input()
-        menu()
-        options()
+        boot()
 
 def status():
     print("Status:")
